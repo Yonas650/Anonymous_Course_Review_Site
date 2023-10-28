5a3cfae151e927da37d243be3d9382c77c12921d
Yonas650 <111200835+Yonas650@users.noreply.github.com>
2023-10-28 17:51:29 -0400

    app.mjs up until part4

--- app.mjs
+++ app.mjs
@@ -1,5 +1,11 @@
+import './config.mjs';
 import express from 'express';
+
 const app = express();
+import './db.mjs';
+import mongoose from 'mongoose';
+const Review = mongoose.model('Review');
+
 
 // set up express static
 import url from 'url';
@@ -10,11 +16,58 @@ app.use(express.static(path.join(__dirname, 'public')));
 // configure templating to hbs
 app.set('view engine', 'hbs');
 
+
 // body parser (req.body)
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('TODO: add / modify routes')
+  let query = {};
+
+  if (req.query.semester && req.query.semester !== 'All') {
+      query.semester = req.query.semester;
+  }
+
+  if (req.query.year) {
+      query.year = req.query.year;
+  }
+
+  if (req.query.professor) {
+      query.professor = new RegExp(req.query.professor, 'i'); // Case-insensitive search
+  }
+
+  Review.find(query)
+      .then(reviews => {
+          res.render('reviewsDisplay', { reviews });
+      })
+      .catch(err => {
+          console.error("Failed to retrieve reviews:", err);
+          res.status(500).send("Error occurred while fetching reviews.");
+      });
 });
+app.get('/reviews/add', (req, res) => {
+    res.render('addReview');  // This will render addReview.hbs
+});
+app.post('/reviews/add', (req, res) => {
+    const { courseNumber, courseName, semester, year, professor, review } = req.body;
+
+    const newReview = new Review({
+        courseNumber,
+        courseName,
+        semester,
+        year,
+        professor,
+        review
+    });
+
+    newReview.save()
+        .then(() => {
+            res.redirect('/'); // Redirect to the home page
+        })
+        .catch(err => {
+            console.error("Failed to save review:", err);
+            res.status(500).send("Error occurred while saving review.");
+        });
+});
+
 
 app.listen(process.env.PORT || 3000);
