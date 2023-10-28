5d3404c187615c3450a6829efe39f98735d2f886
Yonas650 <111200835+Yonas650@users.noreply.github.com>
2023-10-28 17:53:25 -0400

    db.mjs setup

--- db.mjs
+++ db.mjs
@@ -1,1 +1,17 @@
-// add your code here!
+//import './config.mjs';
+import mongoose from 'mongoose';
+
+
+console.log(process.env.DSN)
+// Connect to MongoDB
+mongoose.connect(process.env.DSN);
+// my schema goes here!
+const Review= new mongoose.Schema({
+    courseNumber: String,
+    courseName: String,
+    semester: String,
+    year: Number,
+    professor: String,
+    review: String
+});
+mongoose.model('Review', Review);
